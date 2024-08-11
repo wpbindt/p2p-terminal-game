0ede4d09c7bd886120d940473337ec9caf6a7cab
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2024-08-11 19:11:08 +0200

    Remove dead code

--- termios_io/termios_input_reader.py
+++ termios_io/termios_input_reader.py
@@ -1,5 +1,4 @@
 import sys
-import termios
 from typing import Callable, Generic
 
 from game import CommandType
@@ -12,17 +11,6 @@ class TermiosInputReader(InputReader[CommandType], Generic[CommandType]):
         command_parser: Callable[[str], CommandType],
     ) -> None:
         self._command_parser = command_parser
-        stdin = sys.stdin.fileno()
-        self._old = termios.tcgetattr(stdin)
-
-    def __enter__(self):
-        stdin = sys.stdin.fileno()
-        new = termios.tcgetattr(stdin)
-        new[3] = new[3] & ~(termios.ECHO | termios.ICANON)
-        termios.tcsetattr(stdin, termios.TCSAFLUSH, new)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        termios.tcsetattr(sys.stdin.fileno(), termios.TCSAFLUSH, self._old)
 
     def get_input(self) -> Command:
         return self._command_parser(
